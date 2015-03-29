681833c7a4d5fbf0af7f820878234a9d71e458f2
Paul Vasilenko <meethemail@mail.ru>
2015-03-29 20:44:54 +0400

    Osisp 2 LAB

---
+++ OSISP/Lab2/Lab2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.XMLTabWindow = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.OpenXMLButton = new System.Windows.Forms.Button();
+            this.NewTabButton = new System.Windows.Forms.Button();
+            this.RemoveTabButton = new System.Windows.Forms.Button();
+            this.XMLTabWindow.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // XMLTabWindow
+            // 
+            this.XMLTabWindow.Controls.Add(this.tabPage1);
+            this.XMLTabWindow.Location = new System.Drawing.Point(12, 12);
+            this.XMLTabWindow.Name = "XMLTabWindow";
+            this.XMLTabWindow.SelectedIndex = 0;
+            this.XMLTabWindow.Size = new System.Drawing.Size(901, 491);
+            this.XMLTabWindow.TabIndex = 0;
+            this.XMLTabWindow.TabStop = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.treeView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(893, 465);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Page 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(6, 6);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(881, 453);
+            this.treeView1.TabIndex = 0;
+            // 
+            // OpenXMLButton
+            // 
+            this.OpenXMLButton.Location = new System.Drawing.Point(919, 34);
+            this.OpenXMLButton.Name = "OpenXMLButton";
+            this.OpenXMLButton.Size = new System.Drawing.Size(98, 23);
+            this.OpenXMLButton.TabIndex = 1;
+            this.OpenXMLButton.Text = "Open XML";
+            this.OpenXMLButton.UseVisualStyleBackColor = true;
+            this.OpenXMLButton.Click += new System.EventHandler(this.OpenXMLButton_Click);
+            // 
+            // NewTabButton
+            // 
+            this.NewTabButton.Location = new System.Drawing.Point(919, 63);
+            this.NewTabButton.Name = "NewTabButton";
+            this.NewTabButton.Size = new System.Drawing.Size(98, 23);
+            this.NewTabButton.TabIndex = 2;
+            this.NewTabButton.Text = "New Tab";
+            this.NewTabButton.UseVisualStyleBackColor = true;
+            this.NewTabButton.Click += new System.EventHandler(this.NewTabButton_Click);
+            // 
+            // RemoveTabButton
+            // 
+            this.RemoveTabButton.Location = new System.Drawing.Point(920, 93);
+            this.RemoveTabButton.Name = "RemoveTabButton";
+            this.RemoveTabButton.Size = new System.Drawing.Size(97, 23);
+            this.RemoveTabButton.TabIndex = 3;
+            this.RemoveTabButton.Text = "Remove Current";
+            this.RemoveTabButton.UseVisualStyleBackColor = true;
+            this.RemoveTabButton.Click += new System.EventHandler(this.RemoveTabButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 531);
+            this.Controls.Add(this.RemoveTabButton);
+            this.Controls.Add(this.NewTabButton);
+            this.Controls.Add(this.OpenXMLButton);
+            this.Controls.Add(this.XMLTabWindow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.XMLTabWindow.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl XMLTabWindow;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button OpenXMLButton;
+        private System.Windows.Forms.Button NewTabButton;
+        private System.Windows.Forms.Button RemoveTabButton;
+    }
+}
+
---
+++ OSISP/Lab2/Lab2/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Xml;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewTabButton_Click(object sender, EventArgs e)
+        {
+
+            int tabNumber = XMLTabWindow.TabCount + 1;
+            TreeView newTreeView = new TreeView();
+            newTreeView.Location = new System.Drawing.Point(6, 6);
+            newTreeView.Name = "treeView1";
+            newTreeView.Size = new System.Drawing.Size(881, 453);
+            newTreeView.TabIndex = 0;
+
+            TabPage newTab = new TabPage();
+            newTab.Controls.Add(newTreeView);
+            newTab.Location = new System.Drawing.Point(4, 22);
+            newTab.Name = "tabPage" + tabNumber;
+            newTab.Padding = new System.Windows.Forms.Padding(3);
+            newTab.Size = new System.Drawing.Size(893, 465);
+            newTab.TabIndex = 0;
+            newTab.Text = "Page "+tabNumber;
+            newTab.UseVisualStyleBackColor = true;
+
+            this.XMLTabWindow.Controls.Add(newTab);
+            this.XMLTabWindow.SelectTab(newTab);
+
+        }
+
+        private TreeView GetTreeViewFromTab(TabPage tabPage)
+        {
+            return (TreeView)tabPage.Controls["treeView1"];
+        }
+
+        private void RemoveTabButton_Click(object sender, EventArgs e)
+        {
+            if (XMLTabWindow.TabPages.Count > 1)
+            {
+                this.XMLTabWindow.TabPages.Remove(XMLTabWindow.SelectedTab);
+                XMLTabWindow.SelectTab(XMLTabWindow.TabPages[XMLTabWindow.TabPages.Count - 1]);
+            }
+            
+        }
+
+        private void OpenXMLButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML Files (.xml)|*.xml";
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                String filename = openFileDialog.FileName;
+                parseXmlToTab(filename, XMLTabWindow.SelectedTab);
+            }
+
+        }
+
+        private void parseXmlToTab(string filename, TabPage currentTab)
+        {
+            TreeView currentTreeView = GetTreeViewFromTab(currentTab);
+            XmlDocument dom = new XmlDocument();
+                dom.Load(filename);
+
+                XmlNode rootXml = dom.FirstChild;
+
+                TreeNode rootNode = new TreeNode();
+                AddChildToNode(rootNode, rootXml);
+                currentTreeView.Nodes.Add(rootNode);
+                currentTreeView.EndUpdate();
+            
+        
+        }
+
+        private void AddChildToNode(TreeNode treeNode, XmlNode xmlNode)
+        {
+            if (xmlNode != null)
+            {
+                treeNode.Text = xmlNode.Name;
+                if (xmlNode.Attributes != null)
+                {
+                    foreach (XmlAttribute attribute in xmlNode.Attributes)
+                    {
+                        TreeNode attrNode = new TreeNode();
+                        attrNode.Text = attribute.LocalName;
+                        attrNode.Nodes.Add(new TreeNode(attribute.Value));
+                        treeNode.Nodes.Add(attrNode);
+                    }
+                }
+
+                foreach (XmlNode child in xmlNode.ChildNodes)
+                {
+                    TreeNode node = new TreeNode();
+                    AddChildToNode(node, child);
+                    treeNode.Nodes.Add(node);
+                }
+
+            }
+
+        }
+
+    }
+
+}
